ed20197f4dec16583462512d2940c4439617e473
baejunwhyung <baejunwhyung111@naver.com>
2022-03-31 23:40:52 +0900

    배열- 메서드 오버로딩

---
+++ day18/src/day18/SumEx.java
@@ -0,0 +1,23 @@
+package day18;
+
+public class SumEx {
+
+	public static void main(String[] args) {
+		sum(10,20);
+		sum(3.14,1.23);
+		sum(10,20,30);
+	}
+	//매서드의 매개변수 타입에 변화(오버로딩)
+	static void sum(int x,int y) {
+		System.out.println(x+y);
+	}
+	static void sum(double x,double y) {
+		System.out.println(x+y);
+	}
+	//매서드의 매개변수 갯수에 변화
+	static void sum(int x,int y,int z) {
+		System.out.println(x+y+z);
+	}
+	
+
+}
