fa8daf6a0a50d75bb8123da0887790996433c2c8
baejunwhyung <baejunwhyung111@naver.com>
2022-04-13 14:52:16 +0900

    추상클래스/인터페이스/내부클래스-내부클래스 복습및마침

---
+++ day28/src/day28/Anonymous3.java
@@ -0,0 +1,30 @@
+package day28;
+
+public class Anonymous3 {
+
+	public static void main(String[] args) {
+		Inter1 it= new Inter1() {
+
+			@Override
+			public void f1() {
+				System.out.println("inter1 imple f1()");
+				
+			
+			}
+			}; 
+			it.f1();
+
+}
+interface Inter1{
+	void f1();
+}
+//일반클래스
+class Inter1Impl implements Inter1{
+
+	@Override
+	public void f1() {
+		
+	}
+	
+}
+}
