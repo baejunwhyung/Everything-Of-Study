cfc62abfbe287f6295f98e42c717aaefbb133c9b
baejunwhyung <baejunwhyung111@naver.com>
2022-04-19 10:30:35 +0900

    기본 API 클래스 알아보기2

---
+++ day32/src/day32/IntegerWrapper.java
@@ -0,0 +1,13 @@
+package day32;
+
+public class IntegerWrapper {
+
+	public static void main(String[] args) {
+		Integer ii=30; //auto boxing
+		System.out.println(ii.intValue());
+		System.out.println(ii.doubleValue());
+		System.out.println(ii.floatValue());
+		System.out.println(ii.toString()+3);
+	}
+
+}
