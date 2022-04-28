a1b823121299431ce0886bf1409662875b8ba1f6
baejunwhyung <baejunwhyung111@naver.com>
2022-04-28 17:08:34 +0900

    컬렉션 프레임 웍2 / set/list

---
+++ day37/src/day37/TimeCheck.java
@@ -0,0 +1,26 @@
+package day37;
+
+import java.util.*;
+
+public class TimeCheck {
+
+	public static void main(String[] args) {
+		ArrayList a1 = new ArrayList();
+		LinkedList ll = new LinkedList();
+		long start = System.currentTimeMillis();
+		
+		for(int i=0;i<100000;i++) {
+			a1.add(0,String.valueOf(1));
+		}
+		long end = System.currentTimeMillis();
+		System.out.println("ArrayList 작업시간:"+(end-start));
+		
+		start =System.currentTimeMillis();
+		for(int i=0;i<100000;i++) {
+			ll.add(0,String.valueOf(i));
+		}
+		end=System.currentTimeMillis();
+		System.out.println("LinkedList 작업시간:"+(end-start));
+	}
+
+}
