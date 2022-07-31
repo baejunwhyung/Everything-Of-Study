b6c84306a9753b4b00c7e1838fa0d66fcadd48f8
baejunwhyung <101807085+baejunwhyung@users.noreply.github.com>
2022-07-31 21:40:57 +0900

    Add files via upload
    섯다 실습

--- 실습 1일차/src/Car.java
+++ 실습 1일차/src/Car.java
@@ -1,88 +1,126 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Car {
-    public static void main (String[] args){
+    public static void main (String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        price pr=new price(2800,3000);
-        int total=0;
-        int num=0;
-        while(true){
-            System.out.println("=====================");
-            System.out.println("기름 넣을 좋류를 고르시오.");
-            System.out.println("1.휘발유");
-            System.out.println("2.경유");
-            System.out.println("=====================");
-            System.out.print("선택하세요:");
-             num=sc.nextInt();
-             if(num==1){
-                 System.out.println("몇 L 넣으시겠습니까?");
-                 System.out.println("1. 1L");
-                 System.out.println("2. 2L");
-                 System.out.println("3. 3L");
-                 System.out.println("4. 4L");
-                 System.out.print("숫자를 입력하세요:");
-                 num=sc.nextInt();
+        price pr = new price(2800, 3000, 5600);
 
-                 if(num==1){
-                     total += pr.gasolinePrice * 1;
-                     System.out.println(total+"원 입니다.");
-                 }
-                 else if(num==2){
-                     total += pr.gasolinePrice *2;
-                     System.out.println(total+"원 입니다.");
-                 }
-                 else if(num==3){
-                     total += pr.gasolinePrice *3;
-                     System.out.println(total+"원 입니다.");
-                 }
-                 else if(num==4){
-                     total += pr.gasolinePrice *4;
-                     System.out.println(total+"원 입니다.");
-                 }
-                 else{
-                     System.out.println("                ");
-                     System.out.println("다시 입력 해주세요.");
-                     System.out.println("                ");
-                     System.out.println("                ");
-                 }
+        int num = 0;
+        while (true) {
+            try {
+                int total = 0;
+                System.out.println("=====================");
+                System.out.println("넣으실 유종을 골라주세요.");
+                System.out.println("1.휘발유");
+                System.out.println("2.경유");
+                System.out.println("3.고급휘발유");
+                System.out.println("=====================");
+                System.out.print("선택하세요:");
+                num = sc.nextInt();
+                if (num == 1) {
+                    System.out.println("몇 L 넣으시겠습니까?");
+                    System.out.println("1. 1L");
+                    System.out.println("2. 2L");
+                    System.out.println("3. 3L");
+                    System.out.println("4. 4L");
+                    System.out.print("숫자를 입력하세요:");
+                    num = sc.nextInt();
+                    for (int i = 1; i < 5; i++) {
+                        if (num == i) {
+                            total += pr.gasolinePrice * 1;
+                            System.out.println(total + "원 입니다.");
+                        } else if (num == i) {
+                            total += pr.gasolinePrice * 2;
+                            System.out.println(total + "원 입니다.");
+                        } else if (num == i) {
+                            total += pr.gasolinePrice * 3;
+                            System.out.println(total + "원 입니다.");
+                        } else if (num == i) {
+                            total += pr.gasolinePrice * 4;
+                            System.out.println(total + "원 입니다.");
+                        }
 
+                    }
+                    if (num >= 5) {
+                        System.out.println("                ");
+                        System.out.println("다시 입력 해주세요.");
+                        System.out.println("                ");
 
+                    }
 
+                }
+                if (num == 2) {
+                    System.out.println("몇 L 넣으시겠습니까?");
+                    System.out.println("1: 1L");
+                    System.out.println("2: 2L");
+                    System.out.println("3: 3L");
+                    System.out.println("4: 4L");
+                    System.out.print("숫자를 입력하세요:");
+                    num = sc.nextInt();
+                    for (int i = 1; i < 5; i++) {
+                        if (num == i) {
+                            total += pr.gasolinePrice * 1;
+                            System.out.println(total + "원 입니다.");
+                        } else if (num == i) {
+                            total += pr.gasolinePrice * 2;
+                            System.out.println(total + "원 입니다.");
+                        } else if (num == i) {
+                            total += pr.gasolinePrice * 3;
+                            System.out.println(total + "원 입니다.");
+                        } else if (num == i) {
+                            total += pr.gasolinePrice * 4;
+                            System.out.println(total + "원 입니다.");
+                        }
+                    }
+                    if (num >= 5) {
+                        System.out.println("                ");
+                        System.out.println("다시 입력 해주세요.");
+                        System.out.println("                ");
+                    }
+                }
+                if (num == 3) {
+                    System.out.println("몇 L 넣으시겠습니까?");
+                    System.out.println("1: 1L");
+                    System.out.println("2: 2L");
+                    System.out.println("3: 3L");
+                    System.out.println("4: 4L");
+                    System.out.print("숫자를 입력하세요:");
+                    num = sc.nextInt();
+                    for (int i = 1; i < 5; i++) {
+                        if (num == i) {
+                            total += pr.gasolinePrice * 1;
+                            System.out.println(total + "원 입니다.");
+                        } else if (num == i) {
+                            total += pr.gasolinePrice * 2;
+                            System.out.println(total + "원 입니다.");
+                        } else if (num == i) {
+                            total += pr.gasolinePrice * 3;
+                            System.out.println(total + "원 입니다.");
+                        } else if (num == i) {
+                            total += pr.gasolinePrice * 4;
+                            System.out.println(total + "원 입니다.");
+                        }
+                    }
 
-             }
-             if(num==2){
-                 System.out.println("몇 L 넣으시겠습니까?");
-                 System.out.println("1: 1L");
-                 System.out.println("2: 2L");
-                 System.out.println("3: 3L");
-                 System.out.println("4: 4L");
-                 System.out.print("숫자를 입력하세요:");
-                 num=sc.nextInt();
-                 if(num==1){
-                     total += pr.gasolinePrice*1;
-                     System.out.println(total+"원 입니다.");
-                 }
-                 else if(num==2){
-                     total += pr.gasolinePrice*2;
-                     System.out.println(total+"원 입니다.");
-                 }
-                 else if(num==3){
-                     total += pr.gasolinePrice*3;
-                     System.out.println(total+"원 입니다.");
-                 }
-                 else if(num==4){
-                     total += pr.gasolinePrice*4;
-                     System.out.println(total+"원 입니다.");
-                 }
-                 else{
-                     System.out.println("                ");
-                     System.out.println("다시 입력 해주세요.");
-                     System.out.println("                ");
-                     System.out.println("                ");
-                 }
-             }
+                    if (num >= 5) {
+                        System.out.println("                ");
+                        System.out.println("다시 입력 해주세요.");
+                        System.out.println("                ");
+                    }
+                }
+
+            }catch(InputMismatchException e){
+            System.out.println(e+"정수가 아닙니다.");
 
         }
+        finally{
+            System.out.println("다시 진행 해 주세요.");
+             break;
+         }
+        }
+
 
 
 
@@ -92,10 +130,15 @@ public class Car {
 class price{
     int gasolinePrice;
     int diselPrice;
+    int premiumGasoline;
 
-    price(int gasolinePrice,int diselPrice){
+    price(int gasolinePrice,int diselPrice,int premiumGasoline){
         this.gasolinePrice=gasolinePrice;
         this.diselPrice=diselPrice;
+        this.premiumGasoline=premiumGasoline;
 
     }
 }
+
+
+
